6c03d96aadcb3e64242f0954f66c2a1911e4b416
valdezkm <kmvaldez2@gmail.com>
2019-04-10 16:16:31 -0400

    Updated to run on command line -Arun

--- packageInstaller.R
+++ packageInstaller.R
@@ -1,81 +1,64 @@
-install.packages("shiny")
-install.packages("shinyjs")
-if (!requireNamespace("BiocManager"))
-  install.packages("BiocManager")
-BiocManager::install("GEOquery")
-BiocManager::install("pd.clariom.s.rat")
-BiocManager::install("clariomsrattranscriptcluster.db")
-BiocManager::install("pd.mogene.2.0.st")
-BiocManager::install("mogene20sttranscriptcluster.db")
-BiocManager::install("pd.hg.u133.plus.2")
-BiocManager::install("hgu133plus2.db")
-BiocManager::install("pd.hugene.2.0.st")
-BiocManager::install("hugene20sttranscriptcluster.db")
-BiocManager::install("pd.clariom.s.human.ht")
-BiocManager::install("clariomshumanhttranscriptcluster.db")
-BiocManager::install("pd.clariom.s.human")
-BiocManager::install("clariomshumantranscriptcluster.db")
-BiocManager::install("pd.clariom.s.mouse.ht")
-BiocManager::install("clariomsmousehttranscriptcluster.db")
-BiocManager::install("pd.clariom.s.mouse")
-BiocManager::install("clariomsmousetranscriptcluster.db")
-BiocManager::install("pd.mouse430.2")
-BiocManager::install("mouse4302.db")
-BiocManager::install("pd.hg.u133a")
-BiocManager::install("hgu133a.db")
-BiocManager::install("pd.hugene.1.0.st.v1")
-BiocManager::install("hugene10sttranscriptcluster.db")
-BiocManager::install("pd.mogene.1.0.st.v1")
-BiocManager::install("mogene10sttranscriptcluster.db")
-BiocManager::install("pd.hg.u133a.2")
-BiocManager::install("hgu133a2.db")
-BiocManager::install("pd.huex.1.0.st.v2")
-BiocManager::install("huex10sttranscriptcluster.db")
-BiocManager::install("pd.hg.u219")
-BiocManager::install("hgu219.db")
-BiocManager::install("pd.mg.u74av2")
-BiocManager::install("mgu74av2.db")
-BiocManager::install("pd.mouse430a.2")
-BiocManager::install("mouse430a2.db")
-BiocManager::install("pd.moe430a")
-BiocManager::install("moe430a.db")
-BiocManager::install("pd.hg.u95av2")
-BiocManager::install("hgu95av2.db")
-BiocManager::install("pd.hta.2.0")
-BiocManager::install("hta20transcriptcluster.db")
-BiocManager::install("pd.moex.1.0.st.v1")
-BiocManager::install("moex10sttranscriptcluster.db")
-BiocManager::install("pd.hg.u133b")
-BiocManager::install("hgu133b.db")
-BiocManager::install("pd.hugene.1.1.st.v1")
-BiocManager::install("hugene11sttranscriptcluster.db")
-BiocManager::install("pd.mogene.1.1.st.v1")
-BiocManager::install("mogene11sttranscriptcluster.db")
-BiocManager::install("limma")
-BiocManager::install("oligo")
-BiocManager::install("geneplotter")
-BiocManager::install("multtest")
-BiocManager::install("annotate")
-BiocManager::install("Biobase")
-BiocManager::install("GSVA")
-BiocManager::install("GSEABase")
-install.packages("gplots")
-install.packages("rgl")
-install.packages("rglwidget")
-install.packages("DT")
-install.packages("getopt")
-install.packages("knitr")
-install.packages("reshape")
-install.packages("RColorBrewer")
-install.packages("mixOmics")
-install.packages("calibrate")
-install.packages("rmarkdown")
-install.packages("ggplot2")
-install.packages("ggfortify")
-install.packages("shinyRGL")
-install.packages("plotly")
-install.packages("htmltools")
-install.packages("heatmaply")
-install.packages("pheatmap")
-install.packages("viridis")
-install.packages("dendsort")
+InstalledPackage <- function(package) 
+{
+  available <- suppressMessages(suppressWarnings(sapply(package, require, quietly = TRUE, character.only = TRUE, warn.conflicts = FALSE)))
+  missing <- package[!available]
+  if (length(missing) > 0) return(FALSE)
+  return(TRUE)
+}
+
+CRANChoosen <- function()
+{
+  return(getOption("repos")["CRAN"] != "@CRAN@")
+}
+
+UsePackage <- function(package, defaultCRANmirror = "http://cran.at.r-project.org") 
+{
+  if(!InstalledPackage(package))
+  {
+    if(!CRANChoosen())
+    {       
+      chooseCRANmirror()
+      if(!CRANChoosen())
+      {
+        options(repos = c(CRAN = defaultCRANmirror))
+      }
+    }
+    
+    suppressMessages(suppressWarnings(install.packages(package)))
+    if(!InstalledPackage(package)) return(FALSE)
+  }
+  return(TRUE)
+}
+
+libraries = c("rgl","rglwidget","DT","getopt",
+              "calibrate","shinyRGL","htmltools",
+              "viridis","dendsort","shiny","shinyjs","gplots","knitr",
+              "reshape","RColorBrewer","mixOmics","rmarkdown","ggplot2","ggfortify",
+              "heatmaply","plotly","pheatmap")
+for(library in libraries) 
+{ 
+  if(!UsePackage(library))
+  {
+    stop("Error!", library)
+  }
+}
+
+listOfBiocPackages = c("GEOquery","pd.clariom.s.rat","clariomsrattranscriptcluster.db",
+                       "pd.mogene.2.0.st","mogene20sttranscriptcluster.db","pd.hg.u133.plus.2",
+                       "hgu133plus2.db","pd.hugene.2.0.st","hugene20sttranscriptcluster.db",
+                       "pd.clariom.s.human.ht","clariomshumanhttranscriptcluster.db",
+                       "pd.clariom.s.human","clariomshumantranscriptcluster.db","pd.clariom.s.mouse.ht",
+                       "clariomsmousehttranscriptcluster.db","pd.clariom.s.mouse","clariomsmousetranscriptcluster.db",
+                       "hugene10sttranscriptcluster.db","pd.mogene.1.0.st.v1","mogene10sttranscriptcluster.db",
+                       "pd.hg.u133a.2","hgu133a2.db","pd.huex.1.0.st.v2","huex10sttranscriptcluster.db",
+                       "pd.hg.u219","hgu219.db","pd.mg.u74av2","mgu74av2.db","pd.mouse430a.2",
+                       "mouse430a2.db","pd.moe430a","moe430a.db","pd.hg.u95av2","hgu95av2.db","pd.hta.2.0",
+                       "hta20transcriptcluster.db","pd.moex.1.0.st.v1","moex10sttranscriptcluster.db","pd.hg.u133b",
+                       "hgu133b.db","pd.hugene.1.1.st.v1","hugene11sttranscriptcluster.db","pd.mogene.1.1.st.v1",
+                       "mogene11sttranscriptcluster.db","oligo","geneplotter","multtest","Biobase",
+                       "GSVA","GSEABase","annotate","limma")
+
+notInstalled = setdiff(listOfBiocPackages, rownames(installed.packages()))
+if( length(notInstalled) ) {
+  BiocManager::install(notInstalled, update = FALSE, version = "3.8")
+}
