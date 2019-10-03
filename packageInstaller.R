de8bf0f03b9b88f4ea697bdfb341dbc1d1226b8f
Skyler Kuhn <kuhnsa@nih.gov>
2019-10-03 17:16:55 -0400

    Fixing Non-interactive CRAN Mirror Selection and adding required packages in Bioconductor list

--- packageInstaller.R
+++ packageInstaller.R
@@ -11,20 +11,17 @@ CRANChoosen <- function()
   return(getOption("repos")["CRAN"] != "@CRAN@")
 }
 
-UsePackage <- function(package, defaultCRANmirror = "http://cran.at.r-project.org") 
+UsePackage <- function(package, defaultCRANmirror = "http://cran.us.r-project.org") 
 {
   if(!InstalledPackage(package))
   {
     if(!CRANChoosen())
-    {       
-      chooseCRANmirror()
-      if(!CRANChoosen())
-      {
-        options(repos = c(CRAN = defaultCRANmirror))
-      }
+    { 
+      #chooseCRANmirror()
+      options(repos = c(CRAN = defaultCRANmirror))
     }
     
-    suppressMessages(suppressWarnings(install.packages(package)))
+    suppressMessages(suppressWarnings(install.packages(package, repos = defaultCRANmirror)))
     if(!InstalledPackage(package)) return(FALSE)
   }
   return(TRUE)
@@ -33,7 +30,7 @@ UsePackage <- function(package, defaultCRANmirror = "http://cran.at.r-project.or
 libraries = c("rgl","rglwidget","DT","getopt",
               "calibrate","shinyRGL","htmltools",
               "viridis","dendsort","shiny","shinyjs","gplots","knitr",
-              "reshape","RColorBrewer","mixOmics","rmarkdown","ggplot2","ggfortify",
+              "reshape","RColorBrewer","rmarkdown","ggplot2","ggfortify",
               "heatmaply","plotly","pheatmap")
 for(library in libraries) 
 { 
@@ -43,15 +40,15 @@ for(library in libraries)
   }
 }
 
-listOfBiocPackages = c("GEOquery","pd.clariom.s.rat","clariomsrattranscriptcluster.db",
-                       "pd.mogene.2.0.st","mogene20sttranscriptcluster.db","pd.hg.u133.plus.2",
-                       "hgu133plus2.db","pd.hugene.2.0.st","hugene20sttranscriptcluster.db",
-                       "pd.clariom.s.human.ht","clariomshumanhttranscriptcluster.db",
+listOfBiocPackages = c("GEOquery","mixOmics","pd.clariom.s.rat","pd.mouse430.2","clariomsrattranscriptcluster.db",
+                       "pd.mogene.2.0.st","mogene20sttranscriptcluster.db","pd.hg.u133.plus.2", "mouse4302.db",
+                       "hgu133plus2.db","pd.hugene.2.0.st","hugene20sttranscriptcluster.db", "pd.hg.u133a",
+                       "pd.clariom.s.human.ht","clariomshumanhttranscriptcluster.db","hgu133a.db",
                        "pd.clariom.s.human","clariomshumantranscriptcluster.db","pd.clariom.s.mouse.ht",
                        "clariomsmousehttranscriptcluster.db","pd.clariom.s.mouse","clariomsmousetranscriptcluster.db",
                        "hugene10sttranscriptcluster.db","pd.mogene.1.0.st.v1","mogene10sttranscriptcluster.db",
                        "pd.hg.u133a.2","hgu133a2.db","pd.huex.1.0.st.v2","huex10sttranscriptcluster.db",
-                       "pd.hg.u219","hgu219.db","pd.mg.u74av2","mgu74av2.db","pd.mouse430a.2",
+                       "pd.hg.u219","hgu219.db","pd.mg.u74av2","mgu74av2.db","pd.mouse430a.2", "pd.hugene.1.0.st.v1",
                        "mouse430a2.db","pd.moe430a","moe430a.db","pd.hg.u95av2","hgu95av2.db","pd.hta.2.0",
                        "hta20transcriptcluster.db","pd.moex.1.0.st.v1","moex10sttranscriptcluster.db","pd.hg.u133b",
                        "hgu133b.db","pd.hugene.1.1.st.v1","hugene11sttranscriptcluster.db","pd.mogene.1.1.st.v1",
